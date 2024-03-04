a3a1016f2f603a6a0b32bced408872cc3f867d62
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-03-04 20:37:13 +0000

    authorized if current user is the owner... to allow if they can update/delete a photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,12 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
+  before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
+
+  def ensure_current_user_is_owner
+    if current_user != @photo.owner
+      redirect_back fallback_location: root_url, alert: "You're not authorized for that."
+    end
+  end
 
   # GET /photos or /photos.json
   def index
