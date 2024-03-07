6f820a9e46b5a71e89ae05ef4666fef8c4c8eef1
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-03-07 19:32:06 +0000

    changed view using pundit

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,16 @@
+# app/policies/user_policy.rb
+
+class UserPolicy
+  attr_reader :current_user, :user
+
+  def initialize(current_user, user)
+    @current_user = current_user
+    @user = user
+  end
+
+  def show?
+    user == current_user ||
+     !user.private? || 
+     user.followers.include?(current_user)
+  end
+end
