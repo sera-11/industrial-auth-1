cb79dbd76d83d863fefcc3b9dcebc28c550a1760
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-03-07 19:27:08 +0000

    pundit helper methods for photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
-  before_action :ensure_user_is_authorized, only: [:show]
+  # before_action :ensure_user_is_authorized, only: [:show]
 
   def ensure_current_user_is_owner
     if current_user != @photo.owner
@@ -16,6 +16,7 @@ class PhotosController < ApplicationController
 
   # GET /photos/1 or /photos/1.json
   def show
+    authorize @photo
   end
 
   # GET /photos/new
@@ -76,9 +77,9 @@ class PhotosController < ApplicationController
       params.require(:photo).permit(:image, :comments_count, :likes_count, :caption, :owner_id)
     end
 
-    def ensure_user_is_authorized
-      if !PhotoPolicy.new(current_user, @photo).show?
-        raise Pundit::NoAuthorizedError, "not allowed"
-      end
-    end
+    # def ensure_user_is_authorized
+    #   if !PhotoPolicy.new(current_user, @photo).show?
+    #     raise Pundit::NoAuthorizedError, "not allowed"
+    #   end
+    # end
 end
