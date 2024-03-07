df00962ad71bfac17e779f27340d7f7031ac9d90
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-03-07 21:10:04 +0000

    follow request policy working

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,6 +1,6 @@
 class FollowRequestsController < ApplicationController
   before_action :set_follow_request, only: %i[ show edit update destroy ]
-  before_action {authorize @follow_request || FollowRequest }
+  before_action {authorize (@follow_request || FollowRequest)}
 
   # GET /follow_requests or /follow_requests.json
   def index
